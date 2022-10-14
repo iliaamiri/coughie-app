875092dc4103de127f729e232d1e88dcb19fb90a
Ilia A. Amiri <iliaabedianamiri@gmail.com>
2022-10-13 18:31:25 -0700

    add storybook boilerplate

--- pages/tailwind.js
+++ pages/tailwind.js
@@ -1,8 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
-
 export default function TailwindDemo() {
   return (
     <h1 className="text-3xl font-bold underline">
