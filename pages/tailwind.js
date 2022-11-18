85678418dd909f00cee8d9aecbf637d430628dd4
Ilia A. Amiri <37903573+iliaamiri@users.noreply.github.com>
2022-11-18 14:58:39 -0800

    wipe out next and replace with preact

--- pages/tailwind.js
+++
@@ -1,7 +0,0 @@
-export default function TailwindDemo() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello <a target={"_blank"} className="text-sky-400" href="https://nextjs.org">Tailwind!</a>
-    </h1>
-  )
-}
