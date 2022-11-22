63ab4c1cb6b306216efa223ec19b193a1de71a00
sjkang930 <91238446+sjkang930@users.noreply.github.com>
2022-11-21 17:19:14 -0800

    made displayBar component

---
+++ src/components/header/myProgress/index.jsx
@@ -0,0 +1,20 @@
+import "./index.css";
+
+export default function MyProgress() {
+  return (
+    <div>
+      <p>Smoke Free</p>
+      <div className="">
+        <p>months</p>
+        <p>Days</p>
+        <p>Hours</p>
+      </div>
+      <div className="">
+        <p>Money Saved</p>
+        <p>e-cigareetes</p>
+        <p>life(days)</p>
+        <p>time saved(days)</p>
+      </div>
+    </div>
+  );
+}
