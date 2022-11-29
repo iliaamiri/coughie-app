1d84abe357e9cde21fb2cf13490b22ed2dd30b24
Lucia-martin <90855217+Lucia-martin@users.noreply.github.com>
2022-11-28 17:07:38 -0800

    added source sans pro and temporary splash

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,16 @@
-import { useState } from "preact/hooks";
-import Header from "./components/header";
-import Home from "./routes/home";
-import { Router } from "preact-router";
-import Tutorial from "./routes/tutorial";
-import Checkin from "./routes/daily/checkin";
-import Plant from "./routes/daily/plant";
-import Achievement from "./routes/achievement";
-import Tracker from "./routes/tracker";
-import Communities from "./routes/communities";
-import Navbar from "./components/navbar";
-import "./app.css";
+import { useState } from 'preact/hooks'
+import Header from './components/header'
+import Home from './routes/home'
+import { Router } from 'preact-router'
+import Tutorial from './routes/tutorial'
+import Checkin from './routes/daily/checkin'
+import Plant from './routes/daily/plant'
+import Achievement from './routes/achievement'
+import Tracker from './routes/tracker'
+import Communities from './routes/communities'
+import Navbar from './components/navbar'
+import Button from './components/button'
+import './app.css'
 
 export function App() {
   return (
@@ -24,9 +25,13 @@ export function App() {
         <Tracker path="/tracker" />
         <Communities path="/communities" />
       </Router>
-   
-    <Navbar></Navbar>
+      <div>
+        <h1>Coughie</h1>
+        <p>"the secret to getting ahead is getting started"</p>
+       
+      </div>
+      <Button text="START"></Button>
+      <Navbar></Navbar>
     </>
-    
-  );
+  )
 }
